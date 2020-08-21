a92cb9c237a524b691bab36b668779120aaa4aee
Igor Krstevski <ikrstevski89@gmail.com>
2020-08-21 18:36:13 -0400

    Redirect homepage to /cats if logged in user

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import router from 'next/router';
 import Header from '../components/Header/Header';
 import { PageWrap } from '../styles/PageWrap';
 import { MainScreenStyles } from '../components/MainScreen/MainScreenStyles';
 import AuthBox from '../components/AuthBox/AuthBox';
+import { getLoggedInUser } from '../services';
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Feed the Cats</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <PageWrap>
-      <Header />
-      <MainScreenStyles>
-        <AuthBox initialScreen="register" />
-      </MainScreenStyles>
-    </PageWrap>
-  </>
-);
+const Home = () => {
+  useEffect(() => {
+    (async () => {
+      const user = await getLoggedInUser();
+      if (user) router.push('/cats');
+    })();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Feed the Cats</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageWrap>
+        <Header />
+        <MainScreenStyles>
+          <AuthBox initialScreen="register" />
+        </MainScreenStyles>
+      </PageWrap>
+    </>
+  );
+};
 
 export default Home;
